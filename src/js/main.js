2e2b61040e7ac3c6dcf50f3ad7a052b2a7c1142a
Василий Боклагов <v.boklagov@altatec.ru>
2022-12-29 02:09:01 +0300

    home page finished

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,26 @@ import $ from 'jquery';
 import "fullpage.js"
 
 $(document).ready(function() {
+  const contraindications = localStorage.getItem('contraindications');
+  if (contraindications == 1) {
+    $(".js-contraindications-block").addClass("is-hidden");
+  }
   $('#fullpage').fullpage(
     {scrollBar: true}
   );
 
   $(".js-video-open").click(()=> {
     popups.video()
-  })
+  });
+
+  $(".js-menu-btn").click(()=> {
+    $(".js-header").toggleClass("is-open");
+  });
+
+  $(".js-contraindications-hidden-btn").click(()=> {
+    $(".js-contraindications-block").addClass("is-hidden");
+    localStorage.setItem('contraindications', 1);
+  });
+
+  $(".js-main").addClass("is-loaded");
 });
